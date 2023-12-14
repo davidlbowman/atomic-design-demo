4fef9b3b63bd08116ab70e986677b8b2098a89bf
David L. Bowman <david@davidlbowman.com>
2023-12-14 13:55:27 -0600

    fixed route in sitemap

--- sitemap.ts
+++ sitemap.ts
@@ -1,22 +1,22 @@
 import { fetchAllBookSlugs } from '@/utils/fetchBooks'
 
 export default async function sitemap() {
-    const baseUrl = 'https://atomic-design-demo.vercel.app/'
+    const baseUrl = 'https://atomic-design-demo.vercel.app'
     const slugs = fetchAllBookSlugs()
     const bookPaths = slugs.map(slug => {
         return {
-            url: baseUrl + slug,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
+            url: `${baseUrl}/books/${slug}`,
+            lastmod: new Date(),
+            changefreq: 'monthly',
             priority: 0.5
         }
     })
 
     return [
         {
-            url: 'https://atomic-design-demo.vercel.app/',
-            lastModified: new Date(),
-            changeFrequency: 'daily',
+            url: baseUrl,
+            lastmod: new Date(),
+            changefreq: 'daily',
             priority: 1
         },
         ...bookPaths
